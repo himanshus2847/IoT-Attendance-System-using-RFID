06f9ee2635624e905ffb08041841a023ebb21934
Himanshu Sharma <himanshus2847@gmail.com>
2022-01-26 13:44:09 +0530

    Update send_data_to_google_sheet.ino

--- send_data_to_google_sheet/send_data_to_google_sheet.ino
+++ send_data_to_google_sheet/send_data_to_google_sheet.ino
@@ -5,15 +5,15 @@
 #include <ESP8266WiFiMulti.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
-
 #include <WiFiClientSecureBearSSL.h>
+
 // Fingerprint for demo URL, expires on June 2, 2021, needs to be updated well before this date
 const uint8_t fingerprint[20] = {0x4a, 0x2e, 0xb2, 0xa8, 0x29, 0x12, 0x9a, 0xca, 0xac, 0xe1, 0xe0, 0xf4, 0xa0, 0x6c, 0x74, 0x4b, 0x4b, 0x7d, 0x5b, 0xab};
 // 4a 2e b2 a8 29 12 9a ca ac e1 e0 f4 a0 6c 74 4b 4b 7d 5b ab
 
-constexpr uint8_t RST_PIN = D3;    // Configurable, see typical pin layout above
-constexpr uint8_t SS_PIN = D4;     // Configurable, see typical pin layout above
-constexpr uint8_t BUZZER = D2;     // Configurable, see typical pin layout above
+#define RST_PIN  D3     // Configurable, see typical pin layout above
+#define SS_PIN   D4     // Configurable, see typical pin layout above
+#define BUZZER   D2     // Configurable, see typical pin layout above
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Instance of the class
 MFRC522::MIFARE_Key key;  
@@ -36,7 +36,7 @@ const String data1 = "https://script.google.com/macros/s/AKfycbwi5Lioy_QXdIw0Snh
 void setup() 
 {
   /* Initialize serial communications with the PC */
-  Serial.begin(115200);
+  Serial.begin(9600);
   // Serial.setDebugOutput(true);
 
   Serial.println();
@@ -53,33 +53,26 @@ void setup()
   WiFi.mode(WIFI_STA);
   
   /* Put your WIFI Name and Password here */
-  WiFiMulti.addAP("dna@Himanshu", "himanshu@1997");
+  WiFiMulti.addAP("iot", "project1234");
 
   /* Set BUZZER as OUTPUT */
   pinMode(BUZZER, OUTPUT);
   /* Initialize SPI bus */
   SPI.begin();
-  /* Initialize MFRC522 Module */
-  mfrc522.PCD_Init();
-  Serial.println(F("Scan a MIFARE 1K Tag to write data..."));
   data2.reserve(18);
 }
 
 void loop()
 {
-  /* Prepare the ksy for authentication */
-  /* All keys are set to FFFFFFFFFFFFh at chip delivery from the factory */
-  for (byte i = 0; i < 6; i++)
-  {
-    key.keyByte[i] = 0xFF;
-  }
+  /* Initialize MFRC522 Module */
+  mfrc522.PCD_Init();
   /* Look for new cards */
   /* Reset the loop if no new card is present on RC522 Reader */
   if ( ! mfrc522.PICC_IsNewCardPresent())
   {
     return;
   }
-  
+  Serial.println("Step 3");
   /* Select one of the cards */
   if ( ! mfrc522.PICC_ReadCardSerial()) 
   {
@@ -87,6 +80,7 @@ void loop()
   }
   /* Read data from the same block */
   Serial.println();
+  Serial.println("Step 4");
   Serial.println(F("Reading last data from RFID..."));
   ReadDataFromBlock(blockNum, readBlockData);
   /* If you want to print the full memory dump, uncomment the next line */
@@ -103,11 +97,15 @@ void loop()
   }
   Serial.println();
   digitalWrite(BUZZER, HIGH);
-  delay(500);
+  delay(200);
+  digitalWrite(BUZZER, LOW);
+  delay(200);
+  digitalWrite(BUZZER, HIGH);
+  delay(200);
   digitalWrite(BUZZER, LOW);
   
   // wait for WiFi connection
-  if ((WiFiMulti.run() == WL_CONNECTED) and !readfromInternet) 
+  if ((WiFiMulti.run() == WL_CONNECTED)) 
   {
     std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
 
@@ -134,18 +132,13 @@ void loop()
         // HTTP header has been send and Server response header has been handled
         Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
         // file found at server
-        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) 
-        {
-          String payload = https.getString();
-          Serial.println(payload);
-          readfromInternet = true;
-        } 
       }
       else 
       {
         Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
       }
       https.end();
+      delay(1000);
     } 
     else 
     {
@@ -155,7 +148,13 @@ void loop()
 }
 
 void ReadDataFromBlock(int blockNum, byte readBlockData[]) 
-{
+{ 
+  /* Prepare the ksy for authentication */
+  /* All keys are set to FFFFFFFFFFFFh at chip delivery from the factory */
+  for (byte i = 0; i < 6; i++)
+  {
+    key.keyByte[i] = 0xFF;
+  }
   /* Authenticating the desired data block for Read access using Key A */
   status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, blockNum, &key, &(mfrc522.uid));
 
